840ce2ec4a0479e09de43706097cf55d005ac4f5
Daniela-Padilha <danielasofiapadilha@gmail.com>
2026-03-27 22:47:39 +0000

    display design complete

--- src/display.c
+++ src/display.c
@@ -15,3 +15,40 @@ void	display_destroy(void)
 	fflush(stdout);
 }
 
+static char *anomaly_to_string(t_anomaly_type type)
+{
+	switch (type)
+	{
+	case ANOMALY_BRADYCARDIA:
+		return ("BRADYCARDIA");
+	case ANOMALY_TACHYCARDIA:
+		return ("TACHYCARDIA");
+	case ANOMALY_HYPOXIA:
+		return ("HYPOXIA");
+	case ANOMALY_HYPERTHERMIA:
+		return ("HYPERTHERMIA");
+	case ANOMALY_HYPOTHERMIA:
+		return ("HYPOTHERMIA");
+	default:
+		return ("OK");
+	}
+}
+
+void display_update(t_display_data *data)
+{
+	printf(TOP_LEFT);
+	printf("╔══════════════════════════════════╗\n");
+	printf("║     " BOLD "Vitals-monitor" RES "          ║\n");
+	printf("╠══════════════════════════════════╣\n");
+	printf("║  Heart Rate: %-19.1f BPM ║\n", data->bpm);
+	printf("║  SpO2:       %-19.1f %%  ║\n", data->max_pair.second.value);
+	printf("║  Temperature:%-19.1f °C  ║\n", data->sht_pair.first.value);
+	printf("║  Humidity:   %-19.1f %%  ║\n", data->sht_pair.second.value);
+	printf("╠══════════════════════════════════╣\n");
+	if (data->state.curr == ANOMALY_NONE)
+    	printf("║  Status: " GRN "%-20s" RES "║\n", "OK");
+	else
+    	printf("║  Status: " RED "%-20s" RES "║\n", anomaly_to_string(data->state.curr));
+	printf("╚══════════════════════════════════╝\n");
+	fflush(stdout);
+}
